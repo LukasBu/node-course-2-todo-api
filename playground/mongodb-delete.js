547157d6c0826223c7fff41fad88a0bd52a428bb
TEC\GP <Georg.Popp@tecalliance.net>
2017-06-29 15:36:57 +0200

    add update script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -33,11 +33,11 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(result); 
     // });
 
-    // db.collection('Users').findOneAndDelete({
-    //     _id: new ObjectID("594cc1a2dc88ab29a81794a1")
-    // }).then((results) => {
-    //     console.log(JSON.stringify(results, undefined, 2));
-    // });
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("594cc1a2dc88ab29a81794a1")
+    }).then((results) => {
+        console.log(JSON.stringify(results, undefined, 2));
+    });
 
     //db.close();
 });
